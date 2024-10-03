c69bd7617fd8271762afb4d10f776e222d6b745a
petrlik lukas <petrlik.lukas>
2024-10-03 10:19:28 +0200

    while check

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def obsah_obdelniku():
     while(True):
         a = input("Zadej stranu a: ")
-        if(not a.isdigit):
+        if(not a.isdigit()):
             print("Neplatný vstup")
             continue
         b = input("Zadej stranu b: ")
-        if(not b.isdigit):
+        if(not b.isdigit()):
             print("Neplatný vstup")
             continue
 
-
+        print(int(a)*int(b))
+        break
+        
+obsah_obdelniku()
