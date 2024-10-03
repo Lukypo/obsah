c936287c34efe78891030668ce398980de4f58be
petrlik lukas <petrlik.lukas>
2024-10-03 10:11:32 +0200

    fce obsah obdelniku

---
+++ main.py
@@ -0,0 +1,12 @@
+def obsah_obdelniku():
+    while(True):
+        a = input("Zadej stranu a: ")
+        if(not a.isdigit):
+            print("Neplatný vstup")
+            continue
+        b = input("Zadej stranu b: ")
+        if(not b.isdigit):
+            print("Neplatný vstup")
+            continue
+
+
